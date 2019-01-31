0aa5085498455a0964e611068fc7dca02470a7ed
unknown <3557056@myuwc.ac.za>
2019-01-31 16:15:07 +0200

    Day3 Homework

---
+++ Day3 Package Descriptions.R
@@ -0,0 +1,19 @@
+#Package descriptions
+#Day3
+#Nasreen Arend
+#31 January 2019
+
+#ggsn package 
+#Adds north symbols and scale bars for maps created with 'ggmap' and 'ggplot'.
+#Adds north symbols (18 options) and scale bars in kilometers to maps in geographic or metric, coordinates created with 'ggplot' or 'ggmap'.
+#Allows scale bar to be customized such as the legend colour, bar fix colour and text colour, font and size.
+#Can change the position and the size of north symbol and scale bar.
+
+#scale package
+#Graphical scales map data to aesthetics, and provide methods for automatically determining breaks and labels for axes and legends.
+#The scale package is composed of interdependent components such as palettes, transformations, bounds, scaling functions and mutables ranges.
+#Palettes, pal for short, describe the useful palettes of aesthetics.
+#Transformations, trans for short, describe common scale transformations, their inverses, and ways of generating breaks and labels.
+#Bounds: various ways of rescaling the data
+#Scaling functions: pull together palettes, bounding functions and transformations to provide a complete pathway from raw data to perceptual properties
+#Mutable ranges: in many graphics pathways, scale ranges can not be computed in a single pass, but must be computed over multiple groups or multiple panels. The mutable ranges (implemented with R's new reference based class) provide a thin layer of mutability to make this task easier.
